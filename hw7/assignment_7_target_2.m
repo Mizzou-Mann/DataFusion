eb0b14ffbffc8d554a14da16f76f190ddaf047a2
Mann <chanmannlim@gmail.com>
2014-11-03 15:03:30 -0600

    add target_2 filter

---
+++ hw7/assignment_7_target_2.m
@@ -0,0 +1,28 @@
+% initialize filter
+t = 0;
+x = zeros(4,1);
+P = diag(ones(1,4) * 10^8); % large covariance P
+result = report();
+
+% open data file
+fid = fopen('Target2.txt');
+while ~feof(fid)
+    tline = fgetl(fid);
+    data = sscanf(tline, '%f');
+    
+    if ~isempty(data)
+        [t_new, z, R] = get_observation(data);
+        [t, x, P] = predict(t, x, P, t_new);
+        [x, P, vx, vs] = update(x, P, z, R);
+        result = result.add_data(vx, vs);
+    end
+end
+% close file
+fclose(fid);
+
+result = result.update_estimate(t, x, P);
+
+result.print_final_estimate();
+result.print_prediction(t + 3600);
+result.plot();
+result.print_innovation_sizes_percentage();
